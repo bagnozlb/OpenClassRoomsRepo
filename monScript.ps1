d630d38615634a5c754c8eaba15c1290551eb64e
bagnozlb <fanch.lebras@gmail.com>
2016-09-22 11:18:52 +0200

    début de code pour télécharger un repo git en powershell

--- monScript.ps1
+++ monScript.ps1
@@ -1,2 +1,13 @@
-$text = "Hello world !"
-Write-Host $text
+Function DownloadGit($gitUrl) {
+	$fileSize = dlFile -targetFile $gitUrl -localFile $localFile
+		if (Test-Path -Path $localFile) {
+				$localFileSize = (Get-Item $localFile).length
+						if ($localFileSize -eq $fileSize) {
+									return $true
+												# return $false
+														}
+															}
+															}
+
+
+															DownloadGit -gitUrl "https://github.com/bagnozlb/OpenClassRoomsRepo.git"

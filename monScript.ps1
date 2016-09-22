78e02a993c309b0e384fd2bc17b133b4f98ead69
bagnozlb <fanch.lebras@gmail.com>
2016-09-22 11:21:33 +0200

    Réindentation du code suite à un copier/coller non judicieux

--- monScript.ps1
+++ monScript.ps1
@@ -1,13 +1,11 @@
 Function DownloadGit($gitUrl) {
 	$fileSize = dlFile -targetFile $gitUrl -localFile $localFile
 		if (Test-Path -Path $localFile) {
-				$localFileSize = (Get-Item $localFile).length
-						if ($localFileSize -eq $fileSize) {
-									return $true
-												# return $false
-														}
-															}
-															}
-
-
-															DownloadGit -gitUrl "https://github.com/bagnozlb/OpenClassRoomsRepo.git"
+			$localFileSize = (Get-Item $localFile).length
+			if ($localFileSize -eq $fileSize) {
+				return $true
+				# return $false
+			}
+		}
+}
+										DownloadGit -gitUrl "https://github.com/bagnozlb/OpenClassRoomsRepo.git"
